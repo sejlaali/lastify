48eba7b2b751806bbdc1a9493b601c57074f0862
Sejla Ali <sejlaali@gmail.com>
2019-07-22 12:54:15 -0400

    moved files one folder up

---
+++ build/precache-manifest.06dc3a07845c50b3565ac152fcadbe77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a40dbb3b06863c276697214169afd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92dc637cbcf39e297c67",
+    "url": "/static/css/main.f48a3671.chunk.css"
+  },
+  {
+    "revision": "3edbc1c1b7eeac9ccf0c",
+    "url": "/static/js/2.cd105923.chunk.js"
+  },
+  {
+    "revision": "92dc637cbcf39e297c67",
+    "url": "/static/js/main.29adb647.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
